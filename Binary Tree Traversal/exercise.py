b1cf47bd46d0b25fd3a283862ce8dae87fc6ed78
Solomia200 <hadiichuk.pn@ucu.edu.ua>
2025-04-28 18:25:17 +0300

    add answer for Binary Tree Traversal

---
+++ Binary Tree Traversal/exercise.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.right  = None
+        self.left = None
+
+
+def pre_order(node):
+    if node is None:
+        return []
+    return [node.data] + pre_order(node.left) + pre_order(node.right)
+
+def in_order(node):
+    if node is None:
+        return []
+    return in_order(node.left) + [node.data] + in_order(node.right)
+
+def post_order(node):
+    if node is None:
+        return []
+    return post_order(node.left) + post_order(node.right) + [node.data]
+
